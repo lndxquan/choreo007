76444ee8ce884d4d91cdea370dfd8653052b6797
lndxquan <lndxquan@gmail.com>
2024-03-20 14:59:54 +0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -7,7 +7,7 @@ const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo.zzx.free.hr';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiOGI5NzI0MDgwZTU1ZTcwMzcwZmI3NDI4NzkyMmYzMWIiLCJ0IjoiOGNlY2VlYzQtYzZiNi00N2VkLThhZjItY2I4MThmMDkxZWJkIiwicyI6Ik5XWTFNV1ZsWm1NdFpEYzJZeTAwWkdSaExUbGtZall0TnpneVpqZ3haVE00WkRBNSJ9';
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiODE2NTM0MWJmMDYwMjBiOTQxN2UwNDk3NWRmZWM1ODAiLCJ0IjoiOThkYTQ4NjEtNDE1Yy00ZmNiLWE3NjktYTUwZDg5ZmJkM2RmIiwicyI6IllqUTRPREJqTldRdFlqTmhNUzAwTnpoa0xXSmxOak10TmpFME56RTNPVFZsWW1ZeSJ9';
 const CFIP = process.env.CFIP || 'government.se';
 const NAME = process.env.NAME || 'Choreo';
 
